d9a829f56f29649568e8ceaf73614b4bb4c1f23e
xrkffgg <xrkffgg@vip.qq.com>
2021-02-23 20:47:49 +0800

    chore: add link

--- daily.js
+++ daily.js
@@ -4,9 +4,9 @@ const fs = require('fs');
 function run() {
   try {
     const readme = fs.readFileSync('./README.md', 'utf-8');
-    const Point = 'Quotations</h3>';
+    const Point = '</a></h3>';
     const index = readme.indexOf(Point);
-    const before = readme.substring(0, index + 15);
+    const before = readme.substring(0, index + 9);
 
     const now = new Date();
     const year = now.getFullYear();
@@ -18,6 +18,7 @@ function run() {
 
     const quotation = fs.readFileSync('./quotations.md', 'utf-8');
     const quotations = quotation.split('\n').filter(it => it.startsWith('-'));
+    console.log(`quotations: ${quotations.length}`);
     const dayily = sample(quotations);
     console.log(`dayily: ${dayily}`);
 
