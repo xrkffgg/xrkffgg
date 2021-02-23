ee211258a0c8b7844cf3672934d969d504b35d62
xrkffgg <xrkffgg@vip.qq.com>
2021-02-23 11:55:01 +0800

    feat: add daily.js

---
+++ daily.js
@@ -0,0 +1,43 @@
+const sample = require('lodash/sample');
+const fs = require('fs');
+
+function run() {
+  try {
+    const readme = fs.readFileSync('./README.md', 'utf-8');
+    const Point = 'Quotations</h3>';
+    const index = readme.indexOf(Point);
+    const before = readme.substring(0, index + 15);
+
+    const now = new Date();
+    const year = now.getFullYear();
+    let month = now.getMonth() + 1;
+    month = month < 10 ? `0${month}` : month;
+    let day = now.getDate();
+    day = day < 10 ? `0${day}` : day;
+    const date = `${year}-${month}-${day}`;
+
+    const quotation = fs.readFileSync('./quotations.md', 'utf-8');
+    const quotations = quotation.split('\n').filter(it => it.startsWith('-'));
+    const dayily = sample(quotations);
+    console.log(`dayily: ${dayily}`);
+
+    const newReadme = `${before}
+
+<kbd>${date}</kbd>
+
+${dayily}
+
+<p align="right">
+<img src="https://visitor-badge.glitch.me/badge?page_id=xrkffgg.xrkffgg" />
+<img src="http://hits.dwyl.com/xrkffgg/xrkffgg.svg" />
+</p>
+`;
+
+    fs.writeFileSync('./README.md', newReadme);
+    console.log('Update Success!');
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+run();
